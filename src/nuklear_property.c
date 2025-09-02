bdbb87fd7ffa25f9041fbae2216859bb6678b00d
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2025-09-02 02:07:39 +0200

    temporary fix for nk_do_property not using NK_STRTOD

--- src/nuklear_property.c
+++ src/nuklear_property.c
@@ -282,7 +282,9 @@ nk_do_property(nk_flags *ws,
             break;
         case NK_PROPERTY_DOUBLE:
             nk_string_float_limit(buffer, NK_MAX_FLOAT_PRECISION);
-            variant->value.d = nk_strtod(buffer, 0);
+            // FIXME: for some reason, this wasn't using the macro
+            //variant->value.d = nk_strtod(buffer, 0);
+            variant->value.d = NK_STRTOD(buffer, 0);
             variant->value.d = NK_CLAMP(variant->min_value.d, variant->value.d, variant->max_value.d);
             break;
         }
